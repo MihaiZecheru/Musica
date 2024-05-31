c601691940a30553981d53c14579586cfbfdac38
Mihai Zecheru <zecheruchris@gmail.com>
2024-05-31 00:42:18 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import logo from './musica.png';
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <img src={ logo } alt="Logo" />
+      <button className="btn btn-primary">Click me</button>
+    </>
+  );
+}
+
+export default App;
