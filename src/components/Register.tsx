9f00fe32996b0bf89045fded4eef0ddbb6baa178
Mihai Zecheru <zecheruchris@gmail.com>
2024-06-17 20:37:01 -0700

    Change page header

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -92,7 +92,7 @@ const Register = () => {
                 <div className="row justify-content-center">
                   <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Join Musica</p>
 
                     <form className="mx-1 mx-md-4">
 
