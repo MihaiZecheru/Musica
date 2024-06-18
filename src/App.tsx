3d044de5b79f11d9e31e32515de47475a92b9d36
Mihai Zecheru <zecheruchris@gmail.com>
2024-06-17 19:05:39 -0700

    Add login page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./mdbootstrap/css/mdb.min.css"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Register from './components/Register';
-import React from 'react';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -12,10 +12,10 @@ function App() {
       <Navbar />
       <div className="content">
         <Routes>
-          {/* <Route path="/" element={ <Home /> }> */}
-          {/* <Route path="/search" element={ <Search /> }> */}
-          {/* <Route path="/queue" element={ <Queue /> }> */}
-          {/* <Route path="/login" element={ <Login /> }> */}
+          {/* <Route path="/" element={ <Home /> } /> */}
+          {/* <Route path="/search" element={ <Search /> } /> */}
+          {/* <Route path="/queue" element={ <Queue /> } /> */}
+          <Route path="/login" element={ <Login /> } />
           <Route path="/register" element={ <Register /> } />
         </Routes>
       </div>
