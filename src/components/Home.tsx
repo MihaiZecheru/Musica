3ef3c3094849fed256469bd1de1fae57cc8b0575
Mihai Zecheru <zecheruchris@gmail.com>
2024-06-29 00:19:47 -0700

    Get data from db for sidebar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,88 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import IPlaylist from "../database-types/IPlaylist";
 import SideNav from "./SideNav";
+import ISong from "../database-types/ISong";
+import supabase from "../config/supabase";
+import GetUser from "../functions/GetUser";
+import { PlaylistID, SongID, UserID } from "../database-types/ID";
+import Loading from "./Loading";
+import { TLikedSongData } from "../database-types/ILikedSong";
 
 const Home = () => {
-  const [playlists, setPlaylists] = useState<IPlaylist[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [userID, setUserID] = useState<string | null>(null);
+  const [likedSongs, setLikedSongs] = useState<TLikedSongData[]>([]);
+  const [playlistLibrary, setPlaylistLibrary] = useState<PlaylistID[]>([]);
+  const [playlists, setPlaylists] = useState<IPlaylist[]>([]);
+  const [queueSongIDs, setQueueSongIDs] = useState<SongID[]>([]);
+  const [queue, setQueue] = useState<ISong[]>([]);
+  const loadingStart = Date.now();
+
+  useEffect(() => {
+    (async () => {
+      const _userID: UserID = (await GetUser()).id as UserID;
+      setUserID(_userID);
+
+      const { data: data_UserMusicLibrary, error: error_UserMusicLibrary } = await supabase
+        .from("UserMusicLibrary")
+        .select()
+        .eq("userID", _userID);
+
+      if (error_UserMusicLibrary) {
+        console.error("error fetching UserMusicLibrary: ", data_UserMusicLibrary);
+        throw data_UserMusicLibrary;
+      }
+
+      const userMusicLibrary = data_UserMusicLibrary![0] as { likedSongs: TLikedSongData[], songQueue: SongID[], playlistLibrary: PlaylistID[] };
+      setLikedSongs(userMusicLibrary.likedSongs);
+      setQueueSongIDs(userMusicLibrary.songQueue);
+      setPlaylistLibrary(userMusicLibrary.playlistLibrary);
+
+      const { data: data_playlists, error: error_playlists } = await supabase
+        .from("Playlists")
+        .select()
+        .in('id', userMusicLibrary.playlistLibrary);
+      
+      if (error_playlists) {
+        console.error("error fetching playlists in library: ", error_playlists);
+        throw error_playlists;
+      }
+        
+      setPlaylists(data_playlists);
+
+      const { data: data_queue, error: error_queue } = await supabase
+        .from("Songs")
+        .select()
+        .in('id', userMusicLibrary.songQueue);
+
+      if (error_queue) {
+        console.error("error fetching queue: ", error_queue);
+        throw error_queue;
+      }
+
+      setQueue(data_queue);
+
+      // check if at least 1000ms have passed since loadingStart
+      if (loadingStart + 1000 < Date.now()) {
+        setLoading(false);
+      } else {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000 - (Date.now() - loadingStart));
+      }
+    })();
+  }, []);
+
+  if (loading) {
+    return (
+      <Loading />
+    );
+  }
+
 
   return (
     <>
-      <SideNav playlists={ playlists }/>
+      <SideNav queue={ queue } playlists={ playlists } />
     </>
   );
 }
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
 import IPlaylist from "../database-types/IPlaylist";
+import ISong from "../database-types/ISong";
+import logo from "../musica.png"
 
 interface Props {
   playlists: IPlaylist[];
+  queue: ISong[];
 }
 
-const SideNav = ({ playlists }: Props) => {
+const SideNav = ({ playlists, queue }: Props) => {
   return (
     <nav
       data-mdb-sidenav-init
@@ -16,101 +20,66 @@ const SideNav = ({ playlists }: Props) => {
       <a
         data-mdb-ripple-init
         className="d-flex justify-content-center py-4 mb-3"
-        style={{ "borderBottom": "2px solid #f5f5f5" }}
+        style={{ "borderBottom": "2px solid #f5f5f5", "maxHeight": "103.62px" }}
         href="#!"
-        data-mdb-ripple-color="primary"
+        data-mdb-ripple-color="#FF5F89"
       >
-        <img
-          id="MDB-logo"
-          src="https://mdbcdn.b-cdn.net/wp-content/uploads/2018/06/logo-mdb-jquery-small.webp"
-          alt="MDB Logo"
-          draggable="false"
-        />
+        <div className="d-flex justify-content-center align-items-center">
+          <img
+            id="musica-logo"
+            src={ logo }
+            alt="Musica Logo"
+            draggable="false"
+            width="25%"
+          />
+          <h1 style={{ "color": "var(--musica-blue-dark)", "marginLeft": ".5rem" }}>Musica</h1>
+        </div>
       </a>
 
       <ul className="sidenav-menu px-2 pb-5">
         <li className="sidenav-item">
+          <span className="sidenav-subheading text-muted">Playlists</span>
           <a className="sidenav-link" href="">
-            <i className="fas fa-tachometer-alt fa-fw me-3"></i><span>Overview</span></a>
+            <i className="fas fa-plus fa-fw me-3"></i><span>Create</span></a>
+        </li>
+        <li className="sidenav-item">
+          <a className="sidenav-link" href="">
+            <i className="fas fa-magnifying-glass fa-fw me-3"></i><span>Discover</span></a>
         </li>
 
         <li className="sidenav-item pt-3">
-          <span className="sidenav-subheading text-muted">Create</span>
-          <a className="sidenav-link" href="">
-            <i className="fas fa-plus fa-fw me-3"></i><span>Project</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-plus fa-fw me-3"></i><span>Database</span></a>
+          <span className="sidenav-subheading text-muted">Library</span>
+          <a className="sidenav-link" role="button">
+            <i className="fas fa-heart fa-fw me-3"></i><span>Liked Songs</span></a>
         </li>
 
-        <li className="sidenav-item pt-3">
-          <span className="sidenav-subheading text-muted">Manage</span>
-          <a className="sidenav-link" href="">
-            <i className="fas fa-cubes fa-fw me-3"></i><span>Projects</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-database fa-fw me-3"></i><span>Databases</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-stream fa-fw me-3"></i><span>Custom domains</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-code-branch fa-fw me-3"></i><span>Repositories</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-users fa-fw me-3"></i><span>Team</span></a>
-        </li>
+        {
+          playlists.reverse().map((playlist: IPlaylist) => (
+            <li className="sidenav-item" key={ playlist.id }>
+              <a className="sidenav-link" role="button">
+                <i className="fas fa-music fa-fw me-3"></i><span className="text-truncate">{ playlist.title }</span>
+              </a>
+            </li>
+          ))
+        }
 
         <li className="sidenav-item pt-3">
-          <span className="sidenav-subheading text-muted">Maintain</span>
-          <a className="sidenav-link" href="">
-            <i className="fas fa-chart-pie fa-fw me-3"></i><span>Analytics</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-sync fa-fw me-3"></i><span>Backups</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-shield-alt fa-fw me-3"></i><span>Security</span></a>
+          <span className="sidenav-subheading text-muted">Queue</span>
+          <Link className="sidenav-link" to="/search">
+            <i className="fas fa-plus fa-fw me-3"></i><span>Add To Queue</span>
+          </Link>
         </li>
 
-        <li className="sidenav-item pt-3">
-          <span className="sidenav-subheading text-muted">Admin</span>
-          <a className="sidenav-link" href="">
-            <i className="fas fa-money-bill fa-fw me-3"></i><span>Billing</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-file-contract fa-fw me-3"></i><span>License</span></a>
-        </li>
-
-        <li className="sidenav-item pt-3">
-          <span className="sidenav-subheading text-muted">Tools</span>
-          <a className="sidenav-link" href="">
-            <i className="fas fa-hand-pointer fa-fw me-3"></i><span>Drag & drop builder</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-code fa-fw me-3"></i><span>Online code editor</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fas fa-copy fa-fw me-3"></i><span>SFTP</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fab fa-jenkins fa-fw me-3"></i><span>Jenkins</span></a>
-        </li>
-        <li className="sidenav-item">
-          <a className="sidenav-link" href="">
-            <i className="fab fa-gitlab fa-fw me-3"></i><span>GitLab</span></a>
-        </li>
+        {
+          queue.length > 0 &&
+          queue.reverse().map((song: ISong) => (
+            <li className="sidenav-item" key={ song.id }>
+              <a className="sidenav-link" role="button">
+                <i className="fas fa-music fa-fw me-3"></i><span className="text-truncate">{ song.title }</span>
+              </a>
+            </li>
+          ))
+        }
       </ul>
     </nav>
   );
