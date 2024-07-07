7456e40580a95415cb65a9705ed9ac3559a28510
Mihai Zecheru <zecheruchris@gmail.com>
2024-07-06 18:08:26 -0700

    Change name and remove prependQueue

--- src/functions/getQueue.ts
+++ src/functions/getQueue.ts
@@ -1,6 +1,5 @@
 import supabase from "../config/supabase";
 import { SongID, UserID } from "../database-types/ID";
-import ISong from "../database-types/ISong";
 import { GetUserID } from "./GetUser";
 
 export async function getQueue(): Promise<SongID[]> {
@@ -17,19 +16,4 @@ export async function getQueue(): Promise<SongID[]> {
   }
 
   return data![0].songQueue;
-}
-
-export async function prependQueue(song: ISong) {
-  const queue: SongID[] = await getQueue();
-  queue.unshift(song.id);
-
-  const { error: error_updateQueue } = await supabase
-    .from("UserMusicLibrary")
-    .update({ songQueue: queue })
-    .eq('userID', await GetUserID());
-
-  if (error_updateQueue) {
-    console.error('error updating queue: ', error_updateQueue);
-    throw error_updateQueue;
-  }
 }
