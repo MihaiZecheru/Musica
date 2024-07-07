45c9f21842c14f77e44a0904b724afcd79a9b868
Mihai Zecheru <zecheruchris@gmail.com>
2024-07-07 00:06:28 -0700

    Fix func

--- src/functions/ExtractAudioURL.ts
+++ src/functions/ExtractAudioURL.ts
@@ -1,9 +1,9 @@
 import { VideoID } from "../database-types/ID";
 
 export default async function ExtractAudioURL(videoID: VideoID): Promise<string> {
-  return await fetch('', {
+  return await fetch('https://us-west2-musica-381004.cloudfunctions.net/ExtractAudioURL', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ videoID })
-  }).then(res => res.json())
+    body: JSON.stringify({ videoID }),
+  }).then(res => res.text());
 }
